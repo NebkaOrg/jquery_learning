c6629b956d745b2245d0f9fb043c9e7c0e8c8e8e
romel <rommelnazarian@gmail.com>
2018-02-12 13:03:24 +0330

    تمرین5

---
+++ تمرین5/mycode.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+    $(document).find('p strong').addClass('bold');
+
+
+$("#message").slideDown('slow');
+
+$("#text").click(function () {
+
+    $("#message").slideUp()
+
+});
+
+});
+
+
